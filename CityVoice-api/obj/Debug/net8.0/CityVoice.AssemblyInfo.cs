0060c843163f6fa74d903b2aabf43f1bbaea3919
Antun Rukavina <antunrukavina8@gmail.com>
2025-11-19 15:02:11 +0100

    Updated Program.cs – enabled Swagger in Production, removed HTTPS redirect

--- CityVoice-api/obj/Debug/net8.0/CityVoice.AssemblyInfo.cs
+++ CityVoice-api/obj/Debug/net8.0/CityVoice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityVoice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ca9be1728a6da063812103d3929fbb90a65928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2145c6bc6c8ab91100ed5c0880ee669f7a2e90c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityVoice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityVoice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
